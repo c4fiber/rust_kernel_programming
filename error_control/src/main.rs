a67cd6bc04b420072b1163cb0ac60329ea41272f
c4fiber <qudcjf153@gmail.com>
2025-05-20 21:59:42 +0900

    learn: unwrap, expect with file open(result)

--- error_control/src/main.rs
+++ error_control/src/main.rs
@@ -1,14 +1,21 @@
 use std::fs::File;
+use std::io::Read;
+use std::io;
 
-fn main() {
-    let result = File::open("test.txt");
-    let f = match result {
+fn read_from_file(path: String) -> Result<String, io::Error> {
+    let mut s = String::new();
+    let mut f = match File::open(path) {
         Ok(f) => f,
-        Err(err) => {
-            println!("Error opening file: {:?}", err);
-            return;
-        },
+        Err(e) => return Err(e),
     };
 
-    println!("File opened successfully: {:?}", f);
+    match f.read_to_string(&mut s) {
+        Ok(_len) => return Ok(s),
+        Err(e) => return Err(e),
+    };
+}
+
+fn main() {
+    let ret = read_from_file(String::from("test.txt")).expect("파일 읽기 중 오류가 발생했습니다.");
+    println!("test.txt: {}", ret);
 }
